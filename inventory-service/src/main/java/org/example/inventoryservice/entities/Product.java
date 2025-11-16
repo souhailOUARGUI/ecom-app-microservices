dc94a311a6e89be1f3a80071c916a0563d678d82
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-16 11:45:58 +0100

    added product entity and repo

--- inventory-service/src/main/java/org/example/inventoryservice/entities/Product.java
+++ inventory-service/src/main/java/org/example/inventoryservice/entities/Product.java
@@ -1,5 +1,18 @@
 package org.example.inventoryservice.entities;
 
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor @AllArgsConstructor @Builder @Getter @Setter @ToString
 public class Product {
+    @Id
+    private String id;
+    private String name;
+    private double price;
+    private int quantity;
+
 
 }

a512b74d3418cf1b325f919aed9ec2a88ff0ef04
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-16 11:00:16 +0100

    added CommandLineRunner to test

--- cutomer-service/src/main/java/org/example/cutomerservice/CutomerServiceApplication.java
+++ cutomer-service/src/main/java/org/example/cutomerservice/CutomerServiceApplication.java
@@ -1,7 +1,13 @@
 package org.example.cutomerservice;
 
+import org.example.cutomerservice.entities.Customer;
+import org.example.cutomerservice.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class CutomerServiceApplication {
@@ -10,4 +16,22 @@ public class CutomerServiceApplication {
         SpringApplication.run(CutomerServiceApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(Customer.builder().name("Souhail").email("souhail@gmail.com").build());
+            customerRepository.save(Customer.builder().name("Salma").email("Salma@gmail.com").build());
+            customerRepository.save(Customer.builder().name("Amine").email("Amine@gmail.com").build());
+
+            customerRepository.findAll().forEach(c -> {
+                System.out.println("*************");
+                System.out.println(c.getId());
+                System.out.println(c.getName());
+                System.out.println(c.getEmail());
+                System.out.println("*************");
+
+
+            });
+        };
+    }
 }

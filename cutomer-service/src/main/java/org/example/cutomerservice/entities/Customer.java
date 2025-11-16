24942bf29126b3865d53676d5ab007a7aaf932e2
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-16 10:53:29 +0100

    created the customer entity

---
+++ cutomer-service/src/main/java/org/example/cutomerservice/entities/Customer.java
@@ -0,0 +1,21 @@
+package org.example.cutomerservice.entities;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Builder @Getter @Setter
+
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+
+}

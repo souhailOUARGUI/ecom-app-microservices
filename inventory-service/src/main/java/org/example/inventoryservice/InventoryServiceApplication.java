12431bc9c5499d0024e51f09b6c38a032c93db05
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-16 11:46:33 +0100

    added commandline runner objects for test

--- inventory-service/src/main/java/org/example/inventoryservice/InventoryServiceApplication.java
+++ inventory-service/src/main/java/org/example/inventoryservice/InventoryServiceApplication.java
@@ -1,7 +1,13 @@
 package org.example.inventoryservice;
 
+import org.example.inventoryservice.entities.Product;
+import org.example.inventoryservice.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class InventoryServiceApplication {
@@ -10,4 +16,20 @@ public class InventoryServiceApplication {
         SpringApplication.run(InventoryServiceApplication.class, args);
     }
 
+
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository){
+        return args -> {
+            productRepository.save(Product.builder().id(UUID.randomUUID().toString())
+                            .name("Laptop").price(4000).quantity(2).build());
+            productRepository.save(Product.builder().id(UUID.randomUUID().toString())
+                    .name("mouse").price(300).quantity(4).build());
+            productRepository.save(Product.builder().id(UUID.randomUUID().toString())
+                    .name("monitor").price(1000).quantity(1).build());
+
+            productRepository.findAll().forEach(p -> {
+                System.out.println(p.toString());
+            });
+        };
+    }
 }

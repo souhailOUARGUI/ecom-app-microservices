96020fc9759f65d116ea67f13802908cf0997653
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-16 10:48:02 +0100

    Initial commit

---
+++ cutomer-service/src/test/java/org/example/cutomerservice/CutomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.cutomerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CutomerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

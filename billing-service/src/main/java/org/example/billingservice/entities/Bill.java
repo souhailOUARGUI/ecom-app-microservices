012ecc270f1a2371acf2a2d97a0033cf3a0bd0ef
SouhailOUARGUI <ouargui550@gmail.com>
2025-11-16 19:50:44 +0100

    added Bill and productItem entities

---
+++ billing-service/src/main/java/org/example/billingservice/entities/Bill.java
@@ -0,0 +1,24 @@
+package org.example.billingservice.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Setter @Getter @Builder
+public class Bill {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Date billingDate;
+    private Long customerId;
+    @OneToMany(mappedBy = "bill")
+    private List<ProductItem> productItems = new ArrayList<>();
+
+}
---
+++ billing-service/src/main/java/org/example/billingservice/entities/ProductItem.java
@@ -0,0 +1,24 @@
+package org.example.billingservice.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor @NoArgsConstructor @Getter @Setter @Builder
+public class ProductItem {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String productId;
+
+    @ManyToOne
+//    @JoinColumn(name = "bill_id")
+    private Bill bill;
+    private int quantity;
+    private double unitPrice;
+
+
+
+}
